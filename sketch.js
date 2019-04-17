bb31c627f69cd8e397ba0c04c88eb60bc237acd2
Meki <you@example.com>
2019-04-17 20:06:36 +0200

    Create grid

--- sketch.js
+++ sketch.js
@@ -4,6 +4,23 @@ let FIELDSIZE = 20;
 let CIRCLELENGTH = CANVASSIZE / FIELDSIZE;
 let ELLIPSENUMBER;
 let start;
+let grid = [];
+
+
+
+function setGridRandomly(randomThreshold, gridSize) {
+  for (i = 0; i < gridSize; i++) {
+    let row = [];
+    for (j = 0; j < gridSize; j++) {
+      if (random(1) > randomThreshold) {
+        row.push(1);
+      } else {
+        row.push(0);
+      }
+    }
+    grid.push(row);
+  }
+}
 
 function setup() {
   // put setup code here
@@ -11,6 +28,7 @@ function setup() {
   ellipseMode(CORNER);
   angleMode(DEGREES);
   start = true;
+  setGridRandomly(0.9, FIELDSIZE);
 }
 
 function draw() {
@@ -18,7 +36,7 @@ function draw() {
     background(0)
     for (i = 0; i < FIELDSIZE; i++) {
       for (j = 0; j < FIELDSIZE; j++) {
-        if (random(1) > 0.8) {
+        if (grid[i][j] == 1) {
           push();
           translate(i * CIRCLELENGTH, j * CIRCLELENGTH);
           fill(255);
