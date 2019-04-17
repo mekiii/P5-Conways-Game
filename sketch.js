24039f40e72ab574aad8d37c78cc90683706dd1e
Meki <you@example.com>
2019-04-17 19:37:00 +0200

    Create canvas, ellipses and place randomly

--- sketch.js
+++ sketch.js
@@ -1,28 +1,33 @@
 //Variables
 let CANVASSIZE = 800;
-let FIELDSIZE = 20
-let CIRCLELENGTH = CANVASSIZE/FIELDSIZE;
-
+let FIELDSIZE = 20;
+let CIRCLELENGTH = CANVASSIZE / FIELDSIZE;
+let ELLIPSENUMBER;
+let start;
 
 function setup() {
   // put setup code here
-  createCanvas(CANVASSIZE,CANVASSIZE, SVG)
-  ellipseMode(CORNER)
-  angleMode(DEGREES)
+  createCanvas(CANVASSIZE, CANVASSIZE, SVG);
+  ellipseMode(CORNER);
+  angleMode(DEGREES);
+  start = true;
 }
 
 function draw() {
-  background(0)
-  
-  for (i=0; i < FIELDSIZE; i++) {
-    for (j=0; j < FIELDSIZE; j++){
-      push()
-      translate(i*CIRCLELENGTH, j*CIRCLELENGTH)
-      fill(255)
-      ellipse(0,0,CIRCLELENGTH,CIRCLELENGTH)
-      pop()
+  if (start) {
+    background(0)
+    for (i = 0; i < FIELDSIZE; i++) {
+      for (j = 0; j < FIELDSIZE; j++) {
+        if (random(1) > 0.8) {
+          push();
+          translate(i * CIRCLELENGTH, j * CIRCLELENGTH);
+          fill(255);
+          ellipse(0, 0, CIRCLELENGTH, CIRCLELENGTH);
+          pop();
+        }
+      }
     }
+    start = false;
   }
-  
-  // put drawing code here
+
 }
