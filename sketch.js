e822eabfa1bf93f95b5fbf5ea60a9eb3af70cd57
Meki <you@example.com>
2019-04-18 13:46:24 +0200

    Implemented conway game of life rules, added drawUpdate

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 //Variables
-let CANVASSIZE = 800;
-let FIELDSIZE = 20;
+let CANVASSIZE = 600;
+let FIELDSIZE = 10;
 let CIRCLELENGTH = CANVASSIZE / FIELDSIZE;
 let ELLIPSENUMBER;
 let start;
 let grid = [];
-
-
+let tempGrid = [];
 
 function setGridRandomly(randomThreshold, gridSize) {
   for (i = 0; i < gridSize; i++) {
@@ -27,25 +26,81 @@ function setup() {
   createCanvas(CANVASSIZE, CANVASSIZE, SVG);
   ellipseMode(CORNER);
   angleMode(DEGREES);
+  frameRate(2);
+  setGridRandomly(0.1, FIELDSIZE);
+  console.log(grid);
   start = true;
-  setGridRandomly(0.9, FIELDSIZE);
 }
 
 function draw() {
   if (start) {
     background(0)
-    for (i = 0; i < FIELDSIZE; i++) {
-      for (j = 0; j < FIELDSIZE; j++) {
-        if (grid[i][j] == 1) {
-          push();
-          translate(i * CIRCLELENGTH, j * CIRCLELENGTH);
-          fill(255);
-          ellipse(0, 0, CIRCLELENGTH, CIRCLELENGTH);
-          pop();
+    drawGrid(grid);
+    tempGrid = grid;
+    start = false;
+  }
+  else{
+    tempgrid = setNewGrid(tempGrid); 
+    drawGrid(tempGrid);
+    console.log(tempGrid);
+  }
+}
+
+
+
+function checkNeighbours(row,col,myGrid) {
+  let counter = 0;
+  for (k = row - 1; k <= row + 1; k++){
+    for ( l = col - 1; l <= col + 1; l++){
+      if ( 0 <= k && k < FIELDSIZE && 0 <= l && l < FIELDSIZE){
+        if (myGrid[k][l] == 1 && (k != row && l != col)){
+          counter++;
         }
       }
     }
-    start = false;
   }
+  return counter;
+}
 
+function setNewGrid(myGrid){
+  let newGrid = myGrid;
+  for (i = 0; i < FIELDSIZE; i++) {
+    for (j = 0; j < FIELDSIZE; j++) {
+      let NumOfNeighbours = checkNeighbours(i,j,newGrid);
+      //Overcrowded
+      if (NumOfNeighbours > 3){
+        newGrid[i][j] = 0;
+      }
+      else if (NumOfNeighbours == 3 ){
+        newGrid[i][j] = 1;
+      }
+      else if (newGrid[i][j]==1 && NumOfNeighbours == 2) {
+        newGrid[i][j] = 1;
+      }
+      else if (NumOfNeighbours <= 1 ){
+        newGrid[i][j] = 0;
+      }
+    }
+  }
+  return newGrid;
+}
+
+function drawGrid(gridArray){
+  background(0)
+    for (i = 0; i < FIELDSIZE; i++) {
+      for (j = 0; j < FIELDSIZE; j++) {
+        if (gridArray[i][j] == 1) {
+          drawDot(i,j);
+        }
+      }
+    }
+    console.log("New Grid drawn!")
+}
+
+function drawDot(row, col){
+  push();
+  translate(col * CIRCLELENGTH, row * CIRCLELENGTH);
+  fill(255);
+  ellipse(0, 0, CIRCLELENGTH, CIRCLELENGTH);
+  pop();
 }
